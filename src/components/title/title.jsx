1969242e5ac0a112fd93a28029e04ecb0a4d9320
Tiago-Estudante <Tiago.palmeira@ba.estudante.senai.br>
2024-03-12 19:30:15 -0300

    mudança da v5 pra v6 nas rotas

--- src/components/title/title.jsx
+++
@@ -1,9 +0,0 @@
-import './Title.css'
-
-function Title (props) {
-    return(
-        <h1 className='title'>{props.children}</h1>
-    )
-}
-
-export default Title
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 
 function AppRoutes() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/sobre" element={<Sobre />}></Route>
@@ -13,8 +14,8 @@ function AppRoutes() {
                 <Route path="/servico" element={<Servico />}></Route>
                 <Route path="/servicos" element={<Servicos />}></Route>
             </Routes>
-        </BrowserRouter>
-    )
+        </Router>
+    );
 }
 
 export default AppRoutes
