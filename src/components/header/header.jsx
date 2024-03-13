b78cf230d6cce28409574334fca7465f9193d1bd
Cezar Nogueira <cezartv1221@gmail.com>
2024-03-13 18:09:29 -0300

    Atualizar o CSS do Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,16 @@ import './Header_module.css'
 function Header () {
     return (
         <header className='header'>
-            <span></span>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Serviços</a>
-                <a href="">Contato</a>
-                <a href="">Sobre mim</a>
+                <div className='conteiner-logo'>
+                    <div className="logo-imagem">
+                        <img className="logo" src="../../public/Ananery.svg" alt="Logo" />
+                    </div>
+                </div>
+            <nav className='menu'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Sobre mim</a></li>
             </nav>
         </header>
     )
