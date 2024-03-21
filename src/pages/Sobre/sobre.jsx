88e248e1edc7f62e8883687308a7e1857a54a18f
Tiago-Estudante <Tiago.palmeira@ba.estudante.senai.br>
2024-03-21 20:01:43 -0300

    Modificar pagina sobre

--- src/pages/Sobre/sobre.jsx
+++ src/pages/Sobre/sobre.jsx
@@ -7,17 +7,23 @@ function Sobre () {
 
             <div className="grid_wrapper_sobre">
 
-                <div className='imagem'>
-                </div>
+                <div className='content_sobre'>
 
-                    <div className='text'>
-                        <h3 id='text-sobre'>Ana Nery de Carvalho é uma Terapeuta de Resultado especializada em ansiedade e depressão, certificada pela TRG.<br/><br/>Sua abordagem personalizada e centrada no cliente visa oferecer soluções eficazes, capacitando indivíduos a superar desafios emocionais e recuperar o equilíbrio.<br/><br/>Entre em contato para iniciar sua jornada de bem-estar.
+                    <div className='conteiner_imagem_sobre'></div>
+
+                    <div className='conteiner_text_sobre'>
+
+                        <h3 id='text-sobre'>
+                        Ana Nery de Carvalho é uma Terapeuta de Resultado especializada em ansiedade e depressão, certificada pela TRG.<br/><br/>Sua abordagem personalizada e centrada no cliente visa oferecer soluções eficazes, capacitando indivíduos a superar desafios emocionais e recuperar o equilíbrio.<br/><br/>Entre em contato para iniciar sua jornada de bem-estar.
                         </h3>
+
                     </div>
 
                 </div>
 
             </div>
+
+        </div>
     );
 }
 
